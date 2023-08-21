f1412c01d1c84c71884e4f938ab66d56bd2646c5
snehithvarma86 <55405536+snehithvarma86@users.noreply.github.com>
2023-08-21 21:53:07 +0530

    5 rounds game

--- func.js
+++ func.js
@@ -1,53 +1,86 @@
-let choice;
-function getComputerChoice()
-{
-    var arr= ['rock','paper','scissor'];
-    
-    var p=arr[Math.floor(Math.random()*3)];
-    return p;
-    
+function rounds() {
+
+    let choice;
+    function getComputerChoice() {
+        var arr = ['rock', 'paper', 'scissor'];
+
+        var p = arr[Math.floor(Math.random() * 3)];
+        return p;
+
+    }
+
+
+    function play(computerchoice, playerchoice) {
+        if (computerchoice == 'rock' && playerchoice == 'paper') {
+            return "computer won!";
+        }
+        else if (computerchoice == 'rock' && playerchoice == 'scissor') {
+            return "player won!";
+        }
+        else if (computerchoice == 'paper' && playerchoice == 'scissor') {
+            return "player won!";
+        }
+        else if (computerchoice == 'paper' && playerchoice == 'rock') {
+            return "computer won!";
+        }
+        else if (computerchoice == 'scissor' && playerchoice == 'paper') {
+            return "computer won!";
+        }
+        else if (computerchoice == 'scissor' && playerchoice == 'rock') {
+            return "player won!";
+        }
+        else if(computerchoice==playerchoice) 
+        {
+            return "its a tie";
+        }
+        else
+        {
+            return "you have entered incorrect";
+        }
+    }
+
+
+    var playerchoice = window.prompt("Enter your choice");
+    playerchoice = playerchoice.toLowerCase();
+    var computerchoice = getComputerChoice();
+
+
+
+    var pt=(play(computerchoice, playerchoice));
+    console.log(pt);
+    return pt;
+
 }
 
-
-function play(computerchoice,playerchoice)
+var i=0;
+while(i<5)
 {
-    if(computerchoice=='rock' && playerchoice=='paper')
+    var pw=0;
+    var cw=0;
+    var r=rounds();
+    if(r=="player won!")
     {
-        return "computer won!";
+        pw+=1;
     }
-    else if(computerchoice=='rock' && playerchoice=='scissor')
+
+    if(r=="computer won!")
     {
-        return "player won!";
+        cw+=1;
     }
-    else if(computerchoice=='paper' && playerchoice=='scissor')
-    {
-        return "player won!";
-    }
-    else if(computerchoice=='paper' && playerchoice=='rock')
-    {
-        return "computer won!";
-    }
-    else if(computerchoice=='scissor' && playerchoice=='paper')
-    {
-        return "computer won!";
-    }
-    else if(computerchoice=='scissor' && playerchoice=='rock')
-    {
-        return "player won!";
-    }
-    else
-    {
-        return "its a tie";
-    }
+    i+=1;
+
+}
+if(pw>cw)
+{
+    console.log("Player won!!!!");
+}
+else if(cw<pw)
+{
+    console.log("Computer won!!");
+}
+else
+{
+    console.log("its a tie :(");
 }
 
-
-var playerchoice=window.prompt("Enter your choice");
-playerchoice=playerchoice.toLowerCase();
-var computerchoice=getComputerChoice();
-
-
-console.log(playerchoice);
-
-console.log(play(computerchoice,playerchoice));
 
